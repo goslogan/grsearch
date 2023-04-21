417c5acba9c6964f0f3c1b466889b7c8e8df8db6
Nic Gibson <nic.gibson@redis.com>
2023-04-21 19:09:50 +0100

    tests for json.clear

--- jsonmisc_test.go
+++ jsonmisc_test.go
@@ -5,9 +5,9 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Misc JSON", Label("misc"), func() {
+var _ = Describe("Clear JSON", Label("misc", "json.clear"), func() {
 
-	It("can clear a simple value", Label("json.set"), func() {
+	It("can clear a simple value", func() {
 		cmd1 := client.JSONSet(ctx, "clear1", "$", `[1]`)
 		Expect(cmd1.Err()).NotTo(HaveOccurred())
 		Expect(cmd1.Val()).To(Equal("OK"))
@@ -17,6 +17,28 @@ var _ = Describe("Misc JSON", Label("misc"), func() {
 		Expect(cmd2.Val()).To(Equal(int64(1)))
 
 		cmd3 := client.JSONGet(ctx, "clear1", "$")
+		Expect(cmd3.Err()).NotTo(HaveOccurred())
+		Expect(cmd3.Val()).To(HaveLen(1))
+		Expect(cmd3.Val()[0]).To(BeAssignableToTypeOf([]interface{}{1}))
+		Expect(cmd3.Val()[0]).To(Equal([]interface{}{}))
+	})
+
+	It("can clear multiple values", func() {
+		cmd1 := client.JSONSet(ctx, "clear2", "$", `{"a": [100, 200, 300, 200], "b": {"a": [100, 200, 300, 200]}}`)
+		Expect(cmd1.Err()).NotTo(HaveOccurred())
+		Expect(cmd1.Val()).To(Equal("OK"))
+
+		cmd2 := client.JSONClear(ctx, "clear2", "$..a")
+		Expect(cmd2.Err()).NotTo(HaveOccurred())
+		Expect(cmd2.Val()).To(Equal(int64(2)))
+
+		cmd3 := client.JSONGet(ctx, "clear2", "$.a")
+		Expect(cmd3.Err()).NotTo(HaveOccurred())
+		Expect(cmd3.Val()).To(HaveLen(1))
+		Expect(cmd3.Val()[0]).To(BeAssignableToTypeOf([]interface{}{1}))
+		Expect(cmd3.Val()[0]).To(Equal([]interface{}{}))
+
+		cmd3 = client.JSONGet(ctx, "clear2", "$.b.a")
 		Expect(cmd3.Err()).NotTo(HaveOccurred())
 		Expect(cmd3.Val()).To(HaveLen(1))
 		Expect(cmd3.Val()[0]).To(BeAssignableToTypeOf([]interface{}{1}))
